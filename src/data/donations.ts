0a358ab73db12336ea5903d79617f7113203ea60
Bruno Pulis <brunopulis@protonmail.com>
2026-02-27 13:14:02 -0300

    add new donations

--- src/data/donations.ts
+++ src/data/donations.ts
@@ -7,6 +7,20 @@ export const donations = [
       "Para destinar doações às famílias afetadas pelas chuvas, acrescente um centavo (R$ 0,01) ao valor. Quer doar R$ 50,00 por exemplo, doe R$ 50,01.",
   },
   {
+    name: "Ajuda Humanitária - Zona da Mata",
+    pixKey: "5971177@vakinha.com.br",
+    pixType: "Email" as const,
+    description:
+      "As fortes chuvas que atingiram a Zona da Mata mineira deixaram milhares de famílias em situação de emergência.",
+  },
+  {
+    name: "Vakinha dos Bastiões",
+    pixKey: "5967105@vakinha.com.br",
+    pixType: "Email" as const,
+    description:
+      "Doe, compartilhe e ajude a levar esperança a quem mais precisa.",
+  },
+  {
     name: "A Igreja do Brasil",
     pixKey: "39.304.681/0001-17",
     pixType: "CNPJ" as const,
@@ -36,5 +50,5 @@ export const donations = [
     pixKey: "29.612.649/0001-45",
     pixType: "CNPJ" as const,
     description: "Envie qualquer valor com o final de 0,06 centavos.",
-  }
+  },
 ];
